ba5abc731a276ec53201f4a2f5dffb5abf694496
Nihat Yıldırım <nihat.yilldirim@gmail.com>
2024-02-21 14:25:50 +0300

    Proje genelinde repository design pattern için kullanılacak repo sınıflarının soyut sınıfları yazıldı, ayrıca unitofwork sınıfının soyut sınıfı yazıldı.

---
+++ src/Services/ServiceCore/ServiceCorePackages/ServiceCore.Application/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceCorePackages.ServiceCore.Domain.Entity;
+
+namespace ServiceCorePackages.ServiceCore.Application.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        public DbSet<TEntity> Table { get; }
+    }
+}
---
+++ src/Services/ServiceCore/ServiceCorePackages/ServiceCore.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,16 @@
+﻿using ServiceCorePackages.ServiceCore.Domain.Entity;
+
+namespace ServiceCorePackages.ServiceCore.Application.Repositories
+{
+    public interface IWriteRepository<TEntity> : IRepository<TEntity>
+        where TEntity : BaseEntity, new()
+    {
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> AddRangeAsync(List<TEntity> entities);
+        bool Remove(TEntity entity);
+        Task<bool> RemoveByIdAsync(Guid id);
+        bool RemoveRange(List<TEntity> entities);
+        bool Update(TEntity entity);
+        bool UpdateRange(List<TEntity> entities);
+    }
+}

3c3dc2a127bb85925cd9aef471a8d58ffd4611f2
Nihat Yıldırım <nihat.yilldirim@gmail.com>
2024-02-21 14:33:11 +0300

    Repository design pattern ile gerçekleştirilen temel işlevler base sınıflar aracılığı ile tanımlandı

---
+++ src/Services/ServiceCore/ServiceCorePackages/ServiceCore.Persistence/Repositories/BaseReadRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceCorePackages.ServiceCore.Application.Repositories;
+using ServiceCorePackages.ServiceCore.Domain.Entity;
+using System.Linq.Expressions;
+
+namespace ServiceCorePackages.ServiceCore.Persistence.Repositories
+{
+    public abstract class BaseReadRepository<TEntity, TContext> : IReadRepository<TEntity>
+        where TEntity : BaseEntity, new()
+        where TContext : DbContext
+    {
+        public BaseReadRepository(TContext context)
+        {
+            Table = context.Set<TEntity>();
+        }
+
+        public DbSet<TEntity> Table { get; }
+
+        public IQueryable<TEntity> GetAll(bool tracing = true)
+        {
+            var query = Table;
+
+            if (!tracing)
+                return query.AsNoTracking();
+
+            return query;
+        }
+
+        public IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> filter, bool tracing = true)
+        {
+            var query = Table.Where(filter).AsQueryable();
+
+            if (!tracing)
+                return query.AsNoTracking();
+
+            return query;
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid id, bool tracing = true)
+        {
+            var query = Table.AsQueryable();
+
+            if (!tracing)
+                query = query.AsNoTracking();
+
+            return await query.SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> filter, bool tracing = true)
+        {
+            var query = Table.AsQueryable();
+
+            if (!tracing)
+                query = query.AsNoTracking();
+
+            return await query.SingleOrDefaultAsync(filter);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await Table.AnyAsync(filter);
+        }
+    }
+}
---
+++ src/Services/ServiceCore/ServiceCorePackages/ServiceCore.Persistence/Repositories/BaseWriteRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using ServiceCorePackages.ServiceCore.Application.Repositories;
+using ServiceCorePackages.ServiceCore.Domain.Entity;
+
+namespace ServiceCorePackages.ServiceCore.Persistence.Repositories
+{
+    public abstract class BaseWriteRepository<TEntity, TContext> : IWriteRepository<TEntity>
+         where TEntity : BaseEntity, new()
+         where TContext : DbContext
+    {
+        public BaseWriteRepository(TContext context)
+        {
+            Table = context.Set<TEntity>();
+        }
+
+        public DbSet<TEntity> Table { get; }
+
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            EntityEntry<TEntity> entityEntry = await Table.AddAsync(entity);
+
+            return entityEntry.State == EntityState.Added;
+        }
+
+        public async Task<bool> AddRangeAsync(List<TEntity> entities)
+        {
+            await Table.AddRangeAsync(entities);
+
+            return true;
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            EntityEntry<TEntity> entityEntry = Table.Remove(entity);
+
+            return entityEntry.State == EntityState.Deleted;
+        }
+
+        public async Task<bool> RemoveByIdAsync(Guid id)
+        {
+            var deletedEntity = await Table.FindAsync(id);
+
+            return Remove(deletedEntity);
+        }
+
+        public bool RemoveRange(List<TEntity> entities)
+        {
+            Table.RemoveRange(entities);
+
+            return true;
+        }
+
+        public bool Update(TEntity entity)
+        {
+            EntityEntry<TEntity> entityEntry = Table.Update(entity);
+
+            return entityEntry.State == EntityState.Modified;
+        }
+
+        public bool UpdateRange(List<TEntity> entities)
+        {
+            Table.UpdateRange(entities);
+
+            return true;
+        }
+    }
+}
